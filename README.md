de38716ca947d194f70ef6cacb062c200f9cdc1c
kylearmour1 <kylejarmour@gmail.com>
2023-02-27 21:15:13 -0800

    getting current data

--- README.md
+++ README.md
@@ -152,3 +152,149 @@ document.getElementById("searchButton").addEventListener("click", function() {
   });
 });
 
+// List of Dom Elements
+var APIkey = '7a7c04d503977a6da6ac8ba6bdb48f18';
+var cityInputEl = document.getElementById('city-input');
+var submitBtn = document.querySelector('.btn');
+var searchedCities = [];
+var previousCityEl = document.getElementById('history');
+var weatherEl = document.querySelector('#results');
+var fivedayEl = document.querySelector('.five-day');
+var clearSearch =  document.getElementById('clearBtn')
+// This function will pull the api, and then return results based off of search
+function getWeather() {
+  
+  var city = cityInputEl.value;
+  var currentDay = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&units=imperial&appid=' + APIkey;
+  fetch(currentDay)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response.status);
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log(data);
+      displayResults(data);
+      fiveDayForecast(data);
+      displayCities();
+    }); 
+    
+   };
+
+// This function displays the current day's weather. It is set to display A name, 
+function displayResults(data) {
+
+   var cityName = data.city.name;
+   var weatherCondition = data.list[0].weather[0].main;
+   var weatherDescription = data.list[0].weather[0].icon;
+   var temperature = data.list[0].main.temp;
+   var windspeed = data.list[0].wind.speed;
+   var humidity = data.list[0].main.humidity
+   var date = new Date().toLocaleDateString();
+   var iconLink ='http://openweathermap.org/img/wn/' + weatherDescription + '@2x.png';
+
+   weatherEl.innerHTML = '<h2>' + cityName + ' '  + date  + "<img src ='"+iconLink+"'/>" + '</h2>' + '<p>Weather:'+ weatherCondition  + '</p>' +'<p>Temperature:' + temperature + '°F</p>' + '<p>Windspeed: ' + windspeed + 'mph</p>' + '<p>Humidity: ' + humidity + '%</p>';
+
+   
+    };
+   // This function retrieves all of the data for the next 5 days. Inside this, you will see that we pull the same results from the current day, but we are splitting the date and time, while also starting i with a number besides 0 to pull a little further down in the array.
+    function fiveDayForecast(data) {
+      
+          console.log(data);
+          var forecastList = data.list;
+          console.log(forecastList);
+          var fiveDayData = [];
+          for (var i = 0; i < forecastList.length; i += 8) {
+
+            var dateAndTime = forecastList[i].dt_txt;
+            var dateObj = new Date(dateAndTime);
+            var month = dateObj.getMonth() + 1;  // months are zero-indexed
+            var day = dateObj.getDate();
+            var year = dateObj.getFullYear();
+            var date = month + '/' + day + '/' + year;
+
+            var temperature = forecastList[i].main.temp;
+            var weatherDescription = forecastList[i].weather[0].description;
+            var windSpeed = forecastList[i].wind.speed;
+            var humidity = forecastList[i].main.humidity;
+            var icon = forecastList[i].weather[0].icon;
+            fiveDayData.push({ date, temperature, weatherDescription, windSpeed, humidity, icon});
+          }
+          
+          displayFiveDayForecast(fiveDayData);
+       
+    }
+    // This function is what will display all of the results from above, while setting the icon pulled from the API itself, matching the current weather.    
+    function displayFiveDayForecast(fiveDayData) {
+      
+      fivedayEl.innerHTML = '';
+      
+      
+
+      var forecastHeader = document.createElement('h2');
+       forecastHeader.classList = ('five-day-header');
+       forecastHeader.innerHTML = '5 Day Forecast';
+       fivedayEl.appendChild(forecastHeader)
+     
+      for (var i = 0; i < fiveDayData.length; i++) {
+        var date = fiveDayData[i].date;
+        var temperature = fiveDayData[i].temperature;
+        var weatherDescription = fiveDayData[i].weatherDescription;
+        var windSpeed = fiveDayData[i].windSpeed;
+        var humidity = fiveDayData[i].humidity;
+        var icon = fiveDayData[i].icon;
+        var iconLink ='http://openweathermap.org/img/wn/' + icon + '@2x.png';
+
+        var dayEl = document.createElement('div');
+        dayEl.classList = ('card-body');
+        dayEl.innerHTML =  '<h3>' + date + '</h3>' + '<p>Temperature: ' + temperature + '°F</p>' + '<p>Weather: ' + weatherDescription + "<img src ='"+iconLink+"'/>" +  '</p>' + '<p>Wind Speed: ' + windSpeed + 'mph</p>' + '<p>Humidity: ' + humidity + '%</p>';
+    
+        
+        fivedayEl.appendChild(dayEl);
+        
+      }
+    };
+
+
+// This listener will start the process of pulling the data from your search. It will then store this function in the local storage inside inspect.
+ submitBtn.addEventListener('click', function() {
+  getWeather();
+  searchedCities.push(cityInputEl.value);
+  localStorage.setItem('searchedCities', JSON.stringify(searchedCities));
+});
+// This function is set so it can store the searched results inside the local storage
+function displayCities() {
+  var storedCities = JSON.parse(localStorage.getItem('searchedCities'));
+  if (storedCities !== null) {
+    searchedCities = storedCities;
+    displaySearchedCities();
+  }
+};
+// This function creates buttons for the searched cities, so you can click on those to check the weather from the searched city again, without having to retype the city.
+function displaySearchedCities() {
+  previousCityEl.innerHTML = '';
+  for (var i = 0; i < searchedCities.length; i++) {
+    var cityBtn = document.createElement('button');
+    cityBtn.classList = 'list-group-item list-group-item-action';
+    cityBtn.textContent = searchedCities[i];
+    cityBtn.addEventListener('click', function() {
+      cityInputEl.value = this.textContent;
+      getWeather();
+    });
+    previousCityEl.appendChild(cityBtn);
+  }
+}
+ clearSearch.addEventListener('click', clearResults)
+  
+ 
+
+
+ function clearResults() {
+  previousCityEl.innerHTML = '';
+  localStorage.removeItem('searchedCities');
+ }
+
+
+
+ // Notes for myself, eventually go back and add the classlist add and remove to git rid of pesky box at the start of the screen
--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,92 @@
 
 
 var city = document.getElementById("cityInput").value;
-var APIKey = "ce2eaa6886e0920747a7ce7a941aa475";
+var APIKey = "e7a29c6f4a5754e864692a14224adc4e";
+
+
+
+
+
 
 var searchButton = document.getElementById("searchButton");
 console.log(searchButton);
 document.getElementById("searchButton").addEventListener("click", function() {
-  
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+  .then(response => response.json())
+  .then(data => {
+     console.log(data);
+
+
+    
+          const temperature = (data.main.temp - 273.15) * 9/5 + 32;
+          const tempF = temperature.toFixed(2);
+          const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
+          const formattedDate = new Date(data.dt * 1000).toLocaleDateString('en-US', dateOptions);
+          const windSpeedMPH = data.wind.speed * 2.237;
+          const humidity = data.main.humidity;
+
+          document.getElementById("current-day").innerHTML += " " 
+            
+          + data.name
+          + "\n"
+          + formattedDate 
+            + "<br> " 
+            + data.weather[0].main 
+            + "<br> " 
+            + "Temp: "
+            + tempF + "°F"
+            + "<br>"
+            +"Wind: "
+            + windSpeedMPH.toFixed(2) + " mph"
+            + "<br>"
+            + "Humidity: "
+            + humidity + "%"
+            + "<br>";
+
+
+
+
+
+
+
+
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
   fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}`)
   .then(response => response.json())
   .then(data => {
      console.log(data);
+
+
      
-     const temperature = (data.list[0].main.temp - 273.15) * 9/5 + 32;
-const tempF = temperature.toFixed(2);
-const date = new Date(data.list[0].dt_txt);
-const dateOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
-const formattedDate = date.toLocaleDateString('en-US', dateOptions);
-const windSpeedMPH = data.list[0].wind.speed * 2.237;
-const humidity = data.list[0].main.humidity;
 
-document.getElementById("current-day").innerHTML = " " 
-     + data.city.name 
-     +"\n"
-     + formattedDate 
-     + "<br> " 
-     + data.list[0].weather[0].main 
-     + "<br> " 
-     + "Temp: "
-     + tempF + "°F"
-     + "<br>"
-     +"Wind: "
-     + windSpeedMPH.toFixed(2) + " mph"
-     + "<br>"
-     + "Humidity: "
-     + humidity + "%"
-     + "<br>"
-
-
-     const temperature1 = (data.list[7].main.temp - 273.15) * 9/5 + 32;
+     const temperature1 = (data.list[0].main.temp - 273.15) * 9/5 + 32;
      const tempF1 = temperature1.toFixed(2);
-     const date1 = new Date(data.list[7].dt_txt);
+     const date1 = new Date(data.list[0].dt_txt);
      const dateOptions1 = { year: 'numeric', month: '2-digit', day: '2-digit' };
      const formattedDate1 = date1.toLocaleDateString('en-US', dateOptions1);
-     const windSpeedMPH1 = data.list[7].wind.speed * 2.237;
-     const humidity1 = data.list[7].main.humidity;
+     const windSpeedMPH1 = data.list[0].wind.speed * 2.237;
+     const humidity1 = data.list[0].main.humidity;
      
      document.getElementById("day-1").innerHTML = " " 
           
           + formattedDate1 
           + "<br> " 
-          + data.list[7].weather[0].main 
+          + data.list[0].weather[0].main 
           + "<br> " 
           + "Temp: "
           + tempF1 + "°F"
@@ -64,19 +100,19 @@ document.getElementById("current-day").innerHTML = " "
 
 
 
-          const temperature2 = (data.list[15].main.temp - 273.15) * 9/5 + 32;
+     const temperature2 = (data.list[8].main.temp - 273.15) * 9/5 + 32;
      const tempF2 = temperature2.toFixed(2);
-     const date2 = new Date(data.list[15].dt_txt);
+     const date2 = new Date(data.list[8].dt_txt);
      const dateOptions2 = { year: 'numeric', month: '2-digit', day: '2-digit' };
      const formattedDate2 = date2.toLocaleDateString('en-US', dateOptions2);
-     const windSpeedMPH2 = data.list[15].wind.speed * 2.237;
-     const humidity2 = data.list[15].main.humidity;
+     const windSpeedMPH2 = data.list[8].wind.speed * 2.237;
+     const humidity2 = data.list[8].main.humidity;
      
      document.getElementById("day-2").innerHTML = " " 
           
           + formattedDate2 
           + "<br> " 
-          + data.list[15].weather[0].main 
+          + data.list[8].weather[0].main 
           + "<br> " 
           + "Temp: "
           + tempF2 + "°F"
@@ -89,19 +125,19 @@ document.getElementById("current-day").innerHTML = " "
           + "<br>";
      
 
-          const temperature3 = (data.list[23].main.temp - 273.15) * 9/5 + 32;
+          const temperature3 = (data.list[16].main.temp - 273.15) * 9/5 + 32;
           const tempF3 = temperature3.toFixed(2);
-          const date3 = new Date(data.list[23].dt_txt);
+          const date3 = new Date(data.list[16].dt_txt);
           const dateOptions3 = { year: 'numeric', month: '2-digit', day: '2-digit' };
           const formattedDate3 = date3.toLocaleDateString('en-US', dateOptions3);
-          const windSpeedMPH3 = data.list[23].wind.speed * 2.237;
-          const humidity3 = data.list[23].main.humidity;
+          const windSpeedMPH3 = data.list[16].wind.speed * 2.237;
+          const humidity3 = data.list[16].main.humidity;
           
           document.getElementById("day-3").innerHTML = " " 
                
                + formattedDate3 
                + "<br> " 
-               + data.list[23].weather[0].main 
+               + data.list[16].weather[0].main 
                + "<br> " 
                + "Temp: "
                + tempF3 + "°F"
@@ -114,19 +150,19 @@ document.getElementById("current-day").innerHTML = " "
                + "<br>";  
 
 
-               const temperature4 = (data.list[31].main.temp - 273.15) * 9/5 + 32;
+               const temperature4 = (data.list[24].main.temp - 273.15) * 9/5 + 32;
                const tempF4 = temperature4.toFixed(2);
-               const date4 = new Date(data.list[31].dt_txt);
+               const date4 = new Date(data.list[24].dt_txt);
                const dateOptions4 = { year: 'numeric', month: '2-digit', day: '2-digit' };
                const formattedDate4 = date4.toLocaleDateString('en-US', dateOptions4);
-               const windSpeedMPH4 = data.list[31].wind.speed * 2.237;
-               const humidity4 = data.list[31].main.humidity;
+               const windSpeedMPH4 = data.list[24].wind.speed * 2.237;
+               const humidity4 = data.list[24].main.humidity;
                
                document.getElementById("day-4").innerHTML = " " 
                     
                     + formattedDate4 
                     + "<br> " 
-                    + data.list[31].weather[0].main 
+                    + data.list[24].weather[0].main 
                     + "<br> " 
                     + "Temp: "
                     + tempF4 + "°F"
@@ -140,7 +176,31 @@ document.getElementById("current-day").innerHTML = " "
                
           
                     
-                
+                    const temperature5 = (data.list[32].main.temp - 273.15) * 9/5 + 32;
+                    const tempF5 = temperature5.toFixed(2);
+                    const date5 = new Date(data.list[32].dt_txt);
+                    const dateOptions5 = { year: 'numeric', month: '2-digit', day: '2-digit' };
+                    const formattedDate5 = date5.toLocaleDateString('en-US', dateOptions5);
+                    const windSpeedMPH5 = data.list[32].wind.speed * 2.237;
+                    const humidity5 = data.list[32].main.humidity;
+                    
+                    document.getElementById("day-5").innerHTML = " " 
+                         
+                         + formattedDate5 
+                         + "<br> " 
+                         + data.list[32].weather[0].main 
+                         + "<br> " 
+                         + "Temp: "
+                         + tempF5 + "°F"
+                         + "<br>"
+                         +"Wind: "
+                         + windSpeedMPH5.toFixed(2) + " mph"
+                         + "<br>"
+                         + "Humidity: "
+                         + humidity5 + "%"
+                         + "<br>";
+                    
+               
      
     
        
